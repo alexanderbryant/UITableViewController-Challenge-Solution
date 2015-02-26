aa7c4a4b2047254d90b8081adffead23ccf0045e
Alex Bryant <alexander@Gyarados.local>
2015-02-26 12:14:27 -0600

    first try

--- ABTableViewController.h
+++ ABTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ABTableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Alex Bryant on 2/26/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ABTableViewController : UITableViewController
 
 @end
-
